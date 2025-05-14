182fe1669ec5ec40f3536fa179666709d33064e0
khoi-ta <khoi.ta@algotrade.vn>
2025-05-14 13:35:31 +0700

    _: add time delay

--- backtesting.py
+++ backtesting.py
@@ -2,17 +2,22 @@
 This is main module for strategy backtesting
 """
 
+import numpy as np
+from datetime import timedelta
 from decimal import Decimal
 from typing import List
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from config.config import BACKTESTING_CONFIG
 from database.data_service import DataService
 from metrics.metric import Metric
 from utils import (
     get_expired_dates,
     from_cash_to_tradeable_contracts,
 )
+
+FEE_PER_POSITION = Decimal(BACKTESTING_CONFIG["fee"]) * Decimal('100') / Decimal('2')
 
 
 class Backtesting:
@@ -40,22 +45,19 @@ class Backtesting:
         self.printable = printable
         self.data_service = DataService()
         self.metric = None
+
         self.inventory = {"BUY": 0, "SELL": 0}
         self.inventory_price = {"BUY": Decimal('0'), "SELL": Decimal('0')}
+
         self.daily_assets: List[Decimal] = [capital]
         self.daily_returns: List[Decimal] = []
-        self.ac_loss = Decimal("0.0")
         self.tracking_dates = []
+        self.daily_inventory = []
 
-        self.daily_buy = 0
-        self.daily_sell = 0
-
-        # bid-ask spread
-        self.bid_spread = Decimal("0.0")
-        self.ask_spread = Decimal("0.0")
-
+        self.old_timestamp = None
         self.bid_price = None
         self.ask_price = None
+        self.ac_loss = Decimal("0.0")
 
     def move_f1_to_f2(self, f1_price, f2_price):
         """
@@ -64,11 +66,11 @@ class Backtesting:
         if self.inventory["BUY"] != 0:
             self.ac_loss += (self.inventory_price["BUY"] - f1_price) * 100
             self.inventory_price["BUY"] = f2_price
-            self.ac_loss += Decimal('0.47') * Decimal('100') * self.inventory["BUY"]
+            self.ac_loss += FEE_PER_POSITION * self.inventory["BUY"]
         elif self.inventory["SELL"] != 0:
             self.ac_loss += (f1_price - self.inventory_price["SELL"]) * 100
             self.inventory_price["SELL"] = f2_price
-            self.ac_loss += Decimal('0.47') * Decimal('100') * self.inventory["SELL"]
+            self.ac_loss += FEE_PER_POSITION * self.inventory["SELL"]
 
     def update_pnl(self, close_price: Decimal):
         """
@@ -115,16 +117,16 @@ class Backtesting:
         while placeable_sell < 0:
             placeable_sell, placeable_buy = self.get_maximum_placeable(price)
             self.inventory["SELL"] -= 1
-            self.ac_loss += (price - self.inventory_price["SELL"]) * 100 + Decimal(
-                '0.47'
-            ) * Decimal('100') / Decimal('2')
+            self.ac_loss += (
+                price - self.inventory_price["SELL"]
+            ) * 100 + FEE_PER_POSITION
 
         while placeable_buy < 0:
             placeable_sell, placeable_buy = self.get_maximum_placeable(price)
             self.inventory["BUY"] -= 1
             self.ac_loss += (self.inventory_price["BUY"] - price) * Decimal(
                 '100'
-            ) + Decimal('0.47') * Decimal('100') / Decimal('2')
+            ) + FEE_PER_POSITION
 
     def get_maximum_placeable(self, inst_price: Decimal):
         """
@@ -160,20 +162,21 @@ class Backtesting:
         """
         placeable_sell, placeable_buy = self.get_maximum_placeable(price)
         if self.bid_price is None or self.ask_price is None:
-            return
+            return 0
 
+        matched_side = 0
         if self.bid_price >= price and self.inventory["BUY"] < placeable_buy:
             if self.inventory["SELL"] == 0:
                 self.inventory_price["BUY"] = (
                     self.inventory_price["BUY"] * self.inventory["BUY"] + price
                 ) / (self.inventory["BUY"] + 1)
                 self.inventory["BUY"] += 1
+                matched_side += 1
             else:
                 self.inventory["SELL"] -= 1
+                matched_side -= 1
                 self.ac_loss -= (self.inventory_price["SELL"] - price) * Decimal('100')
-
-            self.ac_loss += Decimal('0.47') * Decimal('100') / Decimal('2')
-            self.daily_buy += 1
+            self.ac_loss += FEE_PER_POSITION
 
         if self.ask_price <= price and self.inventory["SELL"] < placeable_sell:
             if self.inventory["BUY"] == 0:
@@ -181,23 +184,34 @@ class Backtesting:
                     self.inventory_price["SELL"] * self.inventory["SELL"] + price
                 ) / (self.inventory["SELL"] + 1)
                 self.inventory["SELL"] += 1
+                matched_side -= 1
             else:
                 self.inventory["BUY"] -= 1
+                matched_side += 1
                 self.ac_loss -= (price - self.inventory_price["BUY"]) * Decimal('100')
+            self.ac_loss += FEE_PER_POSITION
 
-            self.ac_loss += Decimal('0.47') * Decimal('100') / Decimal('2')
-            self.daily_sell += 1
+        return matched_side
 
-    def update_bid_ask(self, price: Decimal, step):
+    def update_bid_ask(self, price: Decimal, step, timestamp):
         """
         Placing bid ask formula
 
         Args:
             price (Decimal)
         """
-        self.handle_matched_order(price)
-        self.bid_price = price - step * (self.inventory["BUY"] + 1)
-        self.ask_price = price + step * (self.inventory["SELL"] + 1)
+        matched_side = self.handle_matched_order(price)
+
+        if self.old_timestamp is None or timestamp > self.old_timestamp + timedelta(
+            seconds=int(BACKTESTING_CONFIG["time"])
+        ):
+            self.old_timestamp = timestamp
+            self.bid_price = price - step * Decimal(self.inventory["BUY"] + 1)
+            self.ask_price = price + step * Decimal(self.inventory["SELL"] + 1)
+        elif matched_side > 0:
+            self.bid_price = price - step * Decimal(self.inventory["BUY"] + 1)
+        elif matched_side < 0:
+            self.ask_price = price + step * Decimal(self.inventory["SELL"] + 1)
 
     def process_data(self, evaluation=False):
         prefix_path = "data/os/" if evaluation else "data/is/"
@@ -260,7 +274,9 @@ class Backtesting:
                 moving_to_f2 = True
 
             self.handle_force_sell(row["f2_price"] if moving_to_f2 else row["price"])
-            self.update_bid_ask(row["f2_price"] if moving_to_f2 else row["price"], step)
+            self.update_bid_ask(
+                row["f2_price"] if moving_to_f2 else row["price"], step, row["datetime"]
+            )
 
             if index == len(data) - 1 or row["date"] != data.iloc[index + 1]["date"]:
                 cur_index += 1
@@ -270,8 +286,15 @@ class Backtesting:
                         f"Realized asset {row['date']}: {int(self.daily_assets[-1] * Decimal('100'))} VND"
                     )
                 moving_to_f2 = False
-                self.tracking_dates.append(row["date"])
                 self.ac_loss = Decimal("0.0")
+                self.bid_price = None
+                self.ask_price = None
+                self.old_timestamp = None
+
+                self.tracking_dates.append(row["date"])
+                self.daily_inventory.append(
+                    self.inventory["BUY"] - self.inventory["SELL"]
+                )
 
         self.metric = Metric(self.daily_returns, None)
 
@@ -321,19 +344,35 @@ class Backtesting:
         plt.grid(True)
         plt.savefig(path, dpi=300, bbox_inches='tight')
 
+    def plot_inventory(self, path="result/backtest/inventory.png"):
+        plt.figure(figsize=(10, 6))
+        plt.plot(
+            self.tracking_dates,
+            self.daily_inventory,
+            label="Portfolio",
+            color='black',
+        )
+
+        plt.title('Inventory Value Over Time')
+        plt.xlabel('Time Step')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(path, dpi=300, bbox_inches='tight')
+
 
 if __name__ == "__main__":
-    import time
-
     bt = Backtesting(
-        capital=Decimal("1e6"),
+        capital=Decimal("1.5e7"),
     )
 
     data = bt.process_data()
-    bt.run(data, Decimal("1.2"))
-    bt.plot_nav()
-    bt.plot_drawdown()
+    bt.run(data, Decimal("0.8"))
+
     print(f"Sharpe ratio: {bt.metric.sharpe_ratio(risk_free_return=Decimal('0.03'))}")
     print(f"Sortino ratio: {bt.metric.sortino_ratio(risk_free_return=Decimal('0.03'))}")
     mdd, _ = bt.metric.maximum_drawdown()
     print(f"Maximum drawdown: {mdd}")
+
+    bt.plot_nav()
+    bt.plot_drawdown()
+    bt.plot_inventory()
